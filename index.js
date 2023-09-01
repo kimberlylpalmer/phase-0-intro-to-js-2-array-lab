bd4aae1738b5684a5fc03626a0708015961c3460
Kimberly L Palmer <kimberlylpalmer@gmail.com>
2023-08-31 18:33:29 -0700

    Finished the arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name)
+};
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat(name) {
+    // cats.splice(2, 0, (name))
+    const copyOfCats = [...cats, (name)]
+return copyOfCats}
+
+function prependCat(name) {
+    const copyOfCats = [(name), ...cats]
+    return copyOfCats
+}
+
+function removeLastCat() {
+    const copyOfCats = [...cats]
+    copyOfCats.splice(2)
+    return copyOfCats
+}
+
+function removeFirstCat() {
+    const copyOfCats = [...cats]
+    copyOfCats.splice(0, 1)
+    return copyOfCats
+}
