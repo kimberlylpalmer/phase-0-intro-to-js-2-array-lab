ad30ce9fa2c2d574f46fb912ac86b152a2664c16
Kimberly Palmer <kimberlylpalmer@gmail.com>
2023-10-31 16:32:29 -0700

    complete Lab 10-31-23

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-const cats = ["Milo", "Otis", "Garfield"];
+const cats = ['Milo', 'Otis', 'Garfield'];
+
 function destructivelyAppendCat(name) {
     cats.push(name)
-};
+}
+
 function destructivelyPrependCat(name) {
     cats.unshift(name)
 }
+
 function destructivelyRemoveLastCat() {
     cats.pop()
 }
+
 function destructivelyRemoveFirstCat() {
     cats.shift()
 }
+
 function appendCat(name) {
-    // cats.splice(2, 0, (name))
-    const copyOfCats = [...cats, (name)]
-return copyOfCats}
+    //cats.splice(2, 0, (name))
+    const newCats = [...cats, (name)]
+    return newCats
+}
 
 function prependCat(name) {
     const copyOfCats = [(name), ...cats]
@@ -23,12 +29,58 @@ function prependCat(name) {
 
 function removeLastCat() {
     const copyOfCats = [...cats]
-    copyOfCats.splice(2)
+    copyOfCats.splice(copyOfCats.length - 1)
     return copyOfCats
 }
 
 function removeFirstCat() {
     const copyOfCats = [...cats]
-    copyOfCats.splice(0, 1)
-    return copyOfCats
+    copyOfCats.splice(0,1)
+    return copyOfCats;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// const cats = ["Milo", "Otis", "Garfield"];
+// function destructivelyAppendCat(name) {
+//     cats.push(name)
+// };
+// function destructivelyPrependCat(name) {
+//     cats.unshift(name)
+// }
+// function destructivelyRemoveLastCat() {
+//     cats.pop()
+// }
+// function destructivelyRemoveFirstCat() {
+//     cats.shift()
+// }
+// function appendCat(name) {
+//     // cats.splice(2, 0, (name))
+//     const copyOfCats = [...cats, (name)]
+// return copyOfCats}
+
+// function prependCat(name) {
+//     const copyOfCats = [(name), ...cats]
+//     return copyOfCats
+// }
+
+// function removeLastCat() {
+//     const copyOfCats = [...cats]
+//     copyOfCats.splice(2)
+//     return copyOfCats
+// }
+
+// function removeFirstCat() {
+//     const copyOfCats = [...cats]
+//     copyOfCats.splice(0, 1)
+//     return copyOfCats
+// }
